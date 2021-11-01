37c7a0e1962844f589f1132a44a4c3ee4d3f2e1a
Eldar Gabdullin <eldargab@gmail.com>
2021-11-01 14:42:32 +0300

    migrate to hydra v5

--- scripts/dev/gen-chain-types.sh
+++ scripts/dev/gen-chain-types.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-mkdir -p chain/default
+mkdir -p src/chain/default
 
 npx ts-node --skip-project node_modules/.bin/polkadot-types-from-defs \
-  --input chain \
-  --package chain
+  --input src/chain \
+  --package "."
