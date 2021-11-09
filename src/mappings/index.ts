bed42c8984a81e2a8e723b15d78ab1f25c9a78d6
dexterslabor <dexterslabor@gmail.com>
2021-11-09 20:37:49 +0100

    feat(handlers): declare events and map handlers

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,70 +1,13 @@
-import {EventContext, StoreContext} from "@subsquid/hydra-common"
-import {Extrinsic} from "../generated/model/extrinsic.model"
-import { Balances } from "../types/balances"
-import handlePostBlock from "../block/handlePostBlock"
-import handleCurrenciesTransferred from "../currencies/handleCurrenciesTransferred";
-import {Tokens} from "../types/tokens"
+import handlePostBlock from "../block/handlePostBlock";
+import handleBalancesTransfer from "../balances/handleBalancesTransfer";
+import handleTokensTransfer from "../tokens/handleTokensTransfer";
+import handlePoolCreated from "../lbp/handlePoolCreated";
+import handlePoolUpdated from "../lbp/handlePoolUpdated";
 
-export async function handleExtrinsicSuccess({
-    block,
-    event,
-    store
-}: StoreContext & EventContext): Promise<void> {
-    let extrinsic = new Extrinsic()
-    extrinsic.id = event.id
-    extrinsic.name = event.extrinsic?.section + '.' + event.extrinsic?.method
-    await store.save(extrinsic)
-}
-
-const handleTokensTransfer = async ({ block, event, store }: any) => {
-    console.log(`tokens.Transfer`)
-    console.log(`method ${event.extrinsic?.method}`)
-    const [currency, from, to, balance] = new Tokens.TransferEvent(event).params;
-    console.log(`tokens transfer of currencyId ${currency} from ${from.toString().substring(0,5)} to ${to.toString().substring(0,5)} balance ${balance}`)
+export {
+    handlePostBlock,
+    handleTokensTransfer,
+    handleBalancesTransfer,
+    handlePoolCreated,
+    handlePoolUpdated
 };
-
-const handleBalanceTransfer = async ({ block, event, store }: any) => {
-    console.log("balance.Transfer");
-    console.log(`method ${event.extrinsic?.method}`)
-    console.log(`arguments ${JSON.stringify(event.extrinsic!.args)}`)
-    const [from, to, balance] = new Balances.TransferEvent(event).params;
-    console.log(`balance transfer of BSX from ${from.toString().substring(0,5)} to ${to.toString().substring(0,5)} balance ${balance}`)
-};
-
-const handleBuyExecuted = async ({ block, event, store }: any) => {
-    console.log("lbp.BuyExecuted");
-    console.log(`arguments of extrinsic resulting in lbp.BuyExecuted ${JSON.stringify(event.extrinsic!.args)}`)
-};
-
-const handleLiquidityAdded = async ({ block, event, store }: any) => {
-    console.log("lbp.LiquidityAdded");
-    console.log(`arguments of extrinsic resulting in lbp.LiquidityAdded ${JSON.stringify(event.extrinsic!.args)}`)
-};
-
-const handleLiquidityRemoved = async ({ block, event, store }: any) => {
-    console.log("lbp.LiquidityRemoved");
-    console.log(`arguments of extrinsic resulting in lbp.LiquidityRemoved ${JSON.stringify(event.extrinsic!.args)}`)
-};
-
-const handleTokensEndowed = async ({ block, event, store }: any) => {
-    console.log("tokens.Endowed");
-    console.log(`arguments of extrinsic: ${event.extrinsic!.args[0].value}`)
-    const [currency, to, balance] = new Tokens.EndowedEvent(event).params;
-    console.log(`tokens endowed of currencyId ${currency} to ${to.toString().substring(0,5)} balance ${balance}`)
-};
-
-const handlePoolCreated = async ({ block, event, store, extrinsic }: EventContext & StoreContext): Promise<void> => {
-    console.log("lbp.PoolCreated");
-    console.log("LBP Pool address",event.params[0].value)
-    console.log("assets", event.params[1].value)
-    //console.log(`arguments of extrinsic resulting in lbp.PoolCreated: ${JSON.stringify(event.extrinsic!.args[0].value)}`)
-};
-
-const handleSellExecuted = async ({ block, event, store }: any) => {
-    console.log("lbp.SellExecuted");
-    console.log(`arguments of extrinsic resulting in lbp.SellExecuted ${JSON.stringify(event.extrinsic!.args)}`)
-    console.log('...')
-};
-
-
-export { handlePostBlock, handleTokensTransfer, handleBalanceTransfer, handleBuyExecuted, handleLiquidityAdded, handleLiquidityRemoved, handleTokensEndowed, handlePoolCreated, handleSellExecuted, handleCurrenciesTransferred };
