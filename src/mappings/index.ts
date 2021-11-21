fc3ac6a6261efb56cd3eb97f9528ce0488f6e232
dexterslabor <dexterslabor@gmail.com>
2021-11-21 23:25:38 +0100

    refactor: 💡 rename handlers

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,7 +1,7 @@
 import handlePostBlock from "./block/handlePostBlock";
 import handleBalancesTransfer from "./balances/handleBalancesTransfer";
 import handleTokensTransfer from "./tokens/handleTokensTransfer";
-import handlePoolCreated from "./lbp/handlePoolCreated";
+import handleLbpPoolCreated from "./lbp/handleLbpPoolCreated";
 import handlePoolUpdated from "./lbp/handlePoolUpdated";
 import handleXykPoolCreated from "./xyk/handleXykPoolCreated";
 import handleCurrentBlockNumbers from "./relayChainInfo/handleCurrentBlockNumbers";
@@ -10,7 +10,7 @@ export {
     handlePostBlock,
     handleTokensTransfer,
     handleBalancesTransfer,
-    handlePoolCreated,
+    handleLbpPoolCreated,
     handlePoolUpdated,
     handleXykPoolCreated,
     handleCurrentBlockNumbers
