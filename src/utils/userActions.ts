f52b6ce42df6aa94aaaac1d809745d417c4ab578
dexterslabor <dexterslabor@users.noreply.github.com>
2021-12-22 17:00:57 +0100

    feat: add xyk sell user action

--- src/utils/userActions.ts
+++ src/utils/userActions.ts
@@ -2,12 +2,13 @@ import { DatabaseManager, SubstrateExtrinsic } from '@subsquid/hydra-common';
 import {
     BuyActionDetail,
     LiquidityAddedActionDetail,
+    SellActionDetail,
     Status,
     UserAction,
     UserActionType,
 } from '../generated/model';
 import { getOrCreate } from './getOrCreate';
-import { buyDetails } from './types';
+import { buyDetails, sellDetails } from './types';
 
 export const createUserActionBuy = async (
     buyDetails: buyDetails,
@@ -19,9 +20,8 @@ export const createUserActionBuy = async (
     Object.assign(buyActionDetail, {
         assetOut: buyDetails.assetOut,
         assetIn: buyDetails.assetIn,
-        assetInAmount: buyDetails.buyPrice * buyDetails.outAmount,
+        assetInAmount: buyDetails.inAmount,
         assetOutAmount: buyDetails.outAmount,
-        buyPrice: buyDetails.buyPrice,
         feeAsset: buyDetails.feeAsset,
         feeAmount: buyDetails.feeAmount,
         poolId: buyDetails.poolId,
@@ -66,6 +66,41 @@ export const createUserActionLiquidityAdded = async (
         account: liquidityAddedDetails.account,
         action: UserActionType.AddLiquidity,
         detail: liquidityAddedActionDetail,
+        parachainBlockHeight,
+    };
+    const userAction = await getOrCreate(
+        store,
+        UserAction,
+        id,
+        userActionValues
+    );
+
+    await store.save(userAction);
+};
+
+export const createUserActionSell = async (
+    sellDetails: sellDetails,
+    extrinsic: SubstrateExtrinsic,
+    parachainBlockHeight: bigint,
+    store: DatabaseManager
+) => {
+    const sellActionDetail = new SellActionDetail();
+    Object.assign(sellActionDetail, {
+        assetIn: sellDetails.assetIn,
+        assetOut: sellDetails.assetOut,
+        assetInAmount: sellDetails.inAmount,
+        assetOutAmount: sellDetails.outAmount,
+        feeAsset: sellDetails.feeAsset,
+        feeAmount: sellDetails.feeAmount,
+        poolId: sellDetails.poolId,
+    });
+
+    const id = extrinsic.id;
+    const userActionValues = {
+        status: Status.isFinalized,
+        account: sellDetails.account,
+        action: UserActionType.Sell,
+        detail: sellActionDetail,
         parachainBlockHeight,
     };
     const userAction = await getOrCreate(
