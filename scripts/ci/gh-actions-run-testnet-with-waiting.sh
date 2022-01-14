ee0039edef5f9883a3566be8064dd5d297d82c49
dexterslabor <dexterslabor@users.noreply.github.com>
2022-01-14 13:21:20 +0100

    ci: add scripts for testnet start

---
+++ scripts/ci/gh-actions-run-testnet-with-waiting.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+chmod -R 777 ./gh-actions-wait-for-log-message.sh
+. ./gh-actions-wait-for-log-message.sh
+
+testnet_logfile=$(mktemp)
+yarn testnet:start 2>&1 | tee "${testnet_logfile}" &
+
+message='POLKADOT LAUNCH COMPLETE'
+echo Basilisk testnet ran success trigger message:: "$message"
+wait_for_log_message "${testnet_logfile}" "$message"
+
+echo ">>> Basilisk testnet has been launched successfully! >>>"
+rm -f "${testnet_logfile}"
