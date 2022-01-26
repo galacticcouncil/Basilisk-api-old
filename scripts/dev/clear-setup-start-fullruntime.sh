5705d92da0f66f318ffe5d0cbeb6a3a2140ab4bd
mckrava <mckrava2@gmail.com>
2022-01-26 16:40:24 +0200

    add custom wait flow for 9988 port

--- scripts/dev/clear-setup-start-fullruntime.sh
+++ scripts/dev/clear-setup-start-fullruntime.sh
@@ -3,8 +3,6 @@ export $(grep -v '^#' .env | xargs)
 
 set -e
 
-yarn global add wait-port
-
 docker network inspect basilisk-wrapper-network >/dev/null 2>&1 || \
     docker network create basilisk-wrapper-network
 
@@ -14,7 +12,7 @@ chmod -R 777 ./scripts/dev/wait-for-log-message.sh
 
 testnet_logfile=$(mktemp)
 
-docker-compose -f ./testnet/testnet-docker-compose.yml -f dockerized-network.yml -p basilisk-testnet up 2>&1 | tee "${testnet_logfile}" &
+docker-compose -f ./testnet/testnet-docker-compose.yml -p basilisk-testnet up 2>&1 | tee "${testnet_logfile}" &
 
 message='POLKADOT LAUNCH COMPLETE'
 echo Basilisk testnet success trigger message:: "$message"
@@ -23,7 +21,12 @@ wait_for_log_message "${testnet_logfile}" "$message"
 echo ">>> Basilisk testnet has been launched successfully! >>>"
 rm -f "${testnet_logfile}"
 
-wait-port 9988
+while ! nc -z localhost 9988; do
+  sleep 1 # wait for 1 second before check again
+  echo "Waiting for port 9988 ..."
+done
+
+echo "Port 9988 is open."
 
 yarn processor:clean-and-setup-single
 
